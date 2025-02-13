217c3d55f220541fe2ba379fc53f3393082189b5
Umesh Verma <umeshverma1999.uv@gmail.com>
2025-02-13 23:21:01 +0530

    Removed unused imports and files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from "next/image";
 import Header from "./home/components/header";
 import HeroAnimatedSlider from "./home/components/hero_animated_slider";
 export default function Home() {
