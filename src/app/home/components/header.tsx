bf68193e974d28a6779dd74e535deb9263578e06
Umesh Verma <umeshverma1999.uv@gmail.com>
2025-03-15 23:23:41 +0530

    Fixed some lint issues

--- src/app/home/components/header.tsx
+++ src/app/home/components/header.tsx
@@ -14,12 +14,10 @@ import {
 import {
     NavigationMenu,
     NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger,
-    NavigationMenuViewport,
     navigationMenuTriggerStyle
 } from "@/components/ui/navigation-menu"
 import ClipAndSpacer from "../../../../public/clip_n_spacer.webp"
@@ -167,7 +165,7 @@ export default function Header() {
                                 <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                                     <li className="row-span-3">
                                         <NavigationMenuLink asChild>
-                                            <a
+                                            <Link
                                                 className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                                                 href="/"
                                             >
@@ -179,7 +177,7 @@ export default function Header() {
                                                     Beautifully designed components built with Radix UI and
                                                     Tailwind CSS.
                                                 </p>
-                                            </a>
+                                            </Link>
                                         </NavigationMenuLink>
                                     </li>
                                     <ListItem href="/docs" title="New">
